fbc60b25a6774a12832aa66bc94e33a30a720549
SandsIII <30729884+SandsIII@users.noreply.github.com>
2019-06-21 09:12:42 -0600

    Added ATR Calc
    Completed adding the ATR calculation for each stcok over a 14 day period

--- getdata.py
+++ getdata.py
@@ -21,13 +21,65 @@ file_counter_ = 1
 # Change the working directory to where the data sits
 os.chdir(dir_)
 
+# Function to calculate the True Range in each row
+def calc_tr_(row):
+
+    high_ = row['High']
+    low_ = row['Low']
+    open_ = row['Open']
+    close_ = row['Close']
+    volume_ = row['Volume']
+    prev_close_ = row['Prev Close']
+
+    max_1_ = high_ - low_
+    max_2_ = abs(high_ - prev_close_)
+    max_3_ = abs(low_- prev_close_)
+
+    TR_ = max(max_1_,max_2_,max_3_)
+
+    return TR_
+
 # Loop through the directory to ingest each file
 for file_ in os.listdir(dir_):
     # Read the csv
     df_stocks_temp_ = pd.read_csv(file_)
     # Grab the ticker
     df_stocks_temp_['Ticker'] = file_.split(".")[0]
-    # If it's the first file populate dataframe 
+    # Populate the previous close
+    df_stocks_temp_['Prev Close'] = df_stocks_temp_['Close'].shift(1)
+    # Calculate the True Range for each row for the ATR calculation
+    df_stocks_temp_['TR'] = df_stocks_temp_.apply(calc_tr_,axis=1)
+    # Create increment for ATR calculation
+    atr_counter_ = 1
+    # Find the length of the TR column then find how many iterations of 14 days 
+    length_atr_ = len(df_stocks_temp_['TR'])
+    iterations_ = length_atr_%14
+
+    # For loop to grab frames of TR data
+    for i in range(0,iterations_):
+        # Grab TR data in increments of 14
+        df_temp_atr_ = df_stocks_temp_['TR'][(atr_counter_-1)*14:atr_counter_*14]
+        # Length of values to iterate and calculate over
+        length_df_temp_atr_ = len(df_temp_atr_)
+        # Loop to Calculate ATR
+        for j in range(0,length_df_temp_atr_):
+            # Grab the True Range value
+            ATR_temp_ = df_temp_atr_[((atr_counter_-1)*14)+j]
+            # If the value is being initializated then populate
+            if i == 0:
+                ATR_ = ATR_temp_
+            # If past initialization then add the TR values for the iterable period
+            else:
+                ATR_ = ATR_ + ATR_temp_
+        # Populate the master data frame with the ATR values for the respective stock 
+        df_stocks_temp_['ATR'] = ATR_
+        # Increment ATR Counter 
+        atr_counter_ += 1
+
+    # Drop TR Column here
+
+    
+    # If the master data frame is being initialized then populate
     if file_counter_ == 1:
         df_stocks_ = df_stocks_temp_
     # Else append dataframe
@@ -43,6 +95,7 @@ for file_ in os.listdir(dir_):
 df_stocks_ = df_stocks_.set_index(['Date','Ticker'])
 # Sort dataframe by date
 df_stocks_ = df_stocks_.sort_values('Date')
+
 
 df = web.get_data_yahoo('AAPL', start=start, end=end)
 #print(df.tail())
